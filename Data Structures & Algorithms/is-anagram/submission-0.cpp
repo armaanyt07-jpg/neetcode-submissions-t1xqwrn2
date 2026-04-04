1b396eaa8086771760a72a2167ab07e1c14c9964
Armaan <armaanyt07@gmail.com>
2026-04-04 09:25:57 +0000

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length()!=t.length())return false;
+        unordered_map<int,int>m1;
+        unordered_map<int,int>m2;
+        for(int i=0;i<s.length();i++)
+        {
+            m1[s[i]]++;
+            m2[t[i]]++;
+        }
+        return m1 == m2;
+    }
+};
